188f624a6944f71f43b7bffd231a0c811ab3ea16
Gogul S <gogul@renambl.com>
2025-07-17 18:27:50 +0530

    created ] folders with initiall files

--- app/config/config.py
+++ app/config/config.py
@@ -6,7 +6,7 @@ dotenv.load_dotenv()
 
 # Load environment variables from .env file
 MONGODB_URL = os.getenv("MONGODB_URL", "mongodb://192.168.1.25:27017")
-DB_NAME = os.getenv("DB_NAME", "renvoiceClient")
+DB_NAME = os.getenv("DB_NAME", "renvoice-client")
 
 
 SERVER_MODE = os.getenv("SERVER_MODE", "DEV")
