98e7bae6cc73405b6a383e045ee559dd0f6d6556
Gogul S <gogul@renambl.com>
2025-07-17 18:08:58 +0530

    ceated databse configuration

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,11 @@
-import os
 from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
-from config.config import MONGODB_URL,DB_NAME
-load_dotenv()
+from app.config.config import DB_NAME,MONGODB_URL
+
 client = AsyncIOMotorClient(MONGODB_URL)
-db = client.get_default_database()
+db = client[DB_NAME]
+
+call_collection='call-logs'
+district_collection='districts'
+org_collection='org'
+campaign_collection='campaign'
+payment_collection='payments'
